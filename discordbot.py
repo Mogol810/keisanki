f66fbf398352934e418e88597f1a871a81a99368
Mogol810 <68849939+Mogol810@users.noreply.github.com>
2020-07-27 21:03:25 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -12,13 +12,13 @@ async def on_ready():
         date = datetime.date.today()
         timedate = datetime.datetime.now()
         now = timedate.strftime('%H:%M')
-        if date.weekday() == 3:
-            if now == '07:00':
+        if date.weekday() == 2:
+            if now == '22:00':
                 channel = client.get_channel(737221401584336916)
                 await channel.send('皆さん計算機概論の復習課題はお済ですか？今日までの課題です。')
                 sleep(60)
-        if date.weekday() == 6:
-            if now == '07:00':
+        if date.weekday() == 5:
+            if now == '22:00':
                 channel = client.get_channel(737221401584336916)
                 await channel.send('皆さん計算機概論の予習課題はお済ですか？明日の授業開始時間までの課題です。')
                 sleep(60)

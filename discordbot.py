f7b765c1726f93f43cf4ffbbc930a13f1a5c95a1
Mogol810 <68849939+Mogol810@users.noreply.github.com>
2020-07-27 20:22:10 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -23,4 +23,4 @@ async def on_ready():
                 
                 
 
-client.run("NzM3MjEwMjAyMDAwMTk1NjU1.Xx6CgA.UtdHTdIzQoJf06OKNfWm3J8_eH4")
+client.run("NzM3MjEwMjAyMDAwMTk1NjU1.Xx6CgA.SGBket0DPJ20uqeYJwUSo-umI_s")

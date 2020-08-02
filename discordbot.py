9c7bd5961e67c427a1ffb01b76d3da0b6d27c649
Mogol810 <68849939+Mogol810@users.noreply.github.com>
2020-08-02 16:33:02 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,28 +1,37 @@
 import discord
-import datetime 
-import os
+import datetime
 from time import sleep
+from discord.ext import tasks
 
-TOKEN = os.environ['DISCORD_BOT_TOKEN']
+
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    while True:
-        date = datetime.date.today()
-        timedate = datetime.datetime.now()
-        now = timedate.strftime('%H:%M')
-        if date.weekday() == 2:
-            if now == '22:00':
-                channel = client.get_channel(737221401584336916)
-                await channel.send('皆さん計算機概論の復習課題はお済ですか？今日までの課題です。')
-                sleep(60)
-        if date.weekday() == 5:
-            if now == '22:00':
-                channel = client.get_channel(737221401584336916)
-                await channel.send('皆さん計算機概論の予習課題はお済ですか？明日の授業開始時間までの課題です。')
-                sleep(60)
+    print('ログインしました')
+    time_check.start()
+
+async def Sendyosyu():
+    channel=client.get_channel(729329464575787088)
+    await channel.send('皆さん計算機概論の復習課題はお済ですか？今日までの課題です。')
+
+async def Sendhukusyu():
+    channel=client.get_channel(729329464575787088)
+    await channel.send('皆さん計算機概論の予習課題はお済ですか？明日の授業時間までの課題です。')
+
+@tasks.loop(seconds=60)
+async def time_check():
+    date = datetime.date.today()
+    timedate = datetime.datetime.now()
+    now = timedate.strftime('%H:%M')
+    if date.weekday() == 3:
+        if now == '08:00':
+            await Sendyosyu()
+            
+    if date.weekday() == 6:
+        if now == '08:33':
+            await Sendhukusyu()
                 
                 
 
-client.run(TOKEN)
+client.run(DISCORD_BOT_TOKEN)
